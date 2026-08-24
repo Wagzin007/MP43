48748a6780b203d2b4da4f2b76e33d86cf164a3c
Wagzin007 <Wagzin007@users.noreply.github.com>
2026-08-24 18:00:02 -0300

    Adicionei mais coisas a interface, ainda tá muito cru mas irei melhorar ainda mais

--- Interface/home_window.py
+++ Interface/home_window.py
@@ -1,14 +1,18 @@
 #O import é essêncial para que a gente desenha a imagem na tela
 import tkinter as tk
 
+#functions lib que serão exênciais durante a execução do código
 #Root tk.Tk abre a janela
 root = tk.Tk()
+def Clicar():
+    clicker = tk.Label(root, text="Botão Clicado")
+    clicker.pack()
 #Define o título da mesma
 appname = root.title("MP43")
 #Define o tamanho máximo e o mínimo da janela
 WindowSize = root.geometry("1280x720"); root.minsize(640, 360)
 Saudation_text = tk.Label(root, text="Seja bem vindo ao MP43"); Saudation_text.pack()
-BotãoExemplo = tk.Button(root, text="Clique"); BotãoExemplo.pack()
-entrada = tk.Entry(root)
+BotãoExemplo = tk.Button(root, text="Clique", command = Clicar); BotãoExemplo.pack()
+entrada = tk.Entry(root); entrada.pack()
 #Faz com que a janela fique aberta
 root.mainloop()
